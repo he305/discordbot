31f9b08ef67c3f7e1868470a957e0081ef2fbeec
he305 <he305@mail.ru>
2018-04-08 21:17:50 +0300

    opus

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from forecast import weather
 import os
 from music import Music
 
-
 BOT_PREFIX = ('?', '!')
 TOKEN = os.environ.get('TOKEN') or 'NDMyNTE5MjMzMDQ2NDQ2MTAx.DauehA.UG5dJPPTyrrTGtVxvRZfsnZDFcQ'
 
