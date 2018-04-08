65a001e74f7981c8b519782440b495391761c7d5
he305 <he305@mail.ru>
2018-04-08 21:12:55 +0300

    opus

--- music.py
+++ music.py
@@ -8,6 +8,7 @@ if not discord.opus.is_loaded():
     # you should replace this with the location the
     # opus library is located in and with the proper filename.
     # note that on windows this DLL is automatically provided for you
+    #bla
     discord.opus.load_opus('opus')
 
 class VoiceEntry:
