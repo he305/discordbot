d28afb75715d021949afa5af5226a8166d5f4fce
he305 <he305@mail.ru>
2018-04-08 21:07:06 +0300

    opus

--- music.py
+++ music.py
@@ -2,13 +2,13 @@ from discord.ext import commands
 import discord
 import asyncio
 
-#if not discord.opus.is_loaded():
+if not discord.opus.is_loaded():
     # the 'opus' library here is opus.dll on windows
     # or libopus.so on linux in the current directory
     # you should replace this with the location the
     # opus library is located in and with the proper filename.
     # note that on windows this DLL is automatically provided for you
-    #discord.opus.load_opus('opus')
+    discord.opus.load_opus('opus')
 
 class VoiceEntry:
     def __init__(self, message, player):
