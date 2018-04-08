b59184de0dfae1dead8b625830115ff2ba2ea5a6
he305 <he305@mail.ru>
2018-04-09 02:19:11 +0300

    eu region

--- music.py
+++ music.py
@@ -109,36 +109,27 @@ class Music:
         sTUBE = ''
         cPL = ''
         final_url = []
-
         if 'list=' in url:
             eq = url.rfind('=') + 1
             cPL = url[eq:]
-
         else:
             print('Incorrect Playlist.')
             exit(1)
-
         try:
             yTUBE = urllib.request.urlopen(url).read()
             sTUBE = str(yTUBE)
         except urllib.error.URLError as e:
             print(e.reason)
-
         tmp_mat = re.compile(r'watch\?v=\S+?list=' + cPL)
         mat = re.findall(tmp_mat, sTUBE)
-
         if mat:
-
             for PL in mat:
                 yPL = str(PL)
                 if '&' in yPL:
                     yPL_amp = yPL.index('&')
                 final_url.append('http://www.youtube.com/' + yPL[:yPL_amp])
-
             all_url = list(set(final_url))
-
             return all_url
-
         else:
             return []
 
